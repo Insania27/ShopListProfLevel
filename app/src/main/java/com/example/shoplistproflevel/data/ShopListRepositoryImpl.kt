3ce89eca23a6d6e9de2fa19c086f0f9e16840e17
Insania27 <imranvuso@mail.ru>
2025-08-12 13:23:49 +0300

    Data layer

---
+++ app/src/main/java/com/example/shoplistproflevel/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,37 @@
+package com.example.shoplistproflevel.data
+
+import com.example.shoplistproflevel.domain.ShopListItem
+import com.example.shoplistproflevel.domain.ShopListRepository
+
+object ShopListRepositoryImpl: ShopListRepository {
+
+    private val shopList = mutableListOf<ShopListItem>()
+
+    private var autoincrementId = 0
+
+    override fun getShopList(): List<ShopListItem> {
+        return shopList.toList()
+    }
+
+    override fun getShopListItemById(shopListItemId: Int): ShopListItem {
+        return shopList.find { it.id == shopListItemId }
+            ?: throw RuntimeException("Element with id $shopListItemId is not found")
+    }
+
+    override fun editShopListItem(shopListItem: ShopListItem) {
+        val oldItem = getShopListItemById(shopListItem.id)
+        shopList.remove(oldItem)
+        shopList.add(shopListItem)
+    }
+
+    override fun deleteShopListItem(shopListItem: ShopListItem) {
+        shopList.remove(shopListItem)
+    }
+
+    override fun addShopListItem(shopListItem: ShopListItem) {
+        if (shopListItem.id == ShopListItem.UNDEFINED_ID){
+            shopListItem.id == autoincrementId++
+        }
+        shopList.add(shopListItem)
+    }
+}
